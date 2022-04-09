2c0883f7c3ad66b06b3f107f77a00eb554523ee5
Kaushik Narasimhan <hcps-narasimhk@henricostudents.org>
2022-04-08 23:48:45 -0400

    worked on admin page

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask_cors import CORS
 conn = psycopg2.connect(
    database="pointofsale", user='postgres', password='pointofsale', host='127.0.0.1', port='5432'
 )
+conn.autocommit = True
 cursor = conn.cursor()
 
 app = Flask(__name__)
@@ -62,21 +63,39 @@ def get_categories():
 def create_order():
     recipientCards = request.form.get('recipient_cards')
     recipientCardJSON = json.loads(recipientCards)
-    print(recipientCardJSON[0])
     customerEmail = str(request.form.get('customer_email'))
     customer = get_users_by_email(customerEmail)
     cursor.execute('insert into orders (customer_users_fk, date_ordered) values (%s, current_timestamp)', (customer[0][0],))
     cursor.execute('select MAX(id) AS orderid FROM orders')
     data = cursor.fetchall()
     orderid = data[0][0]
+    if orderid == 0:
+        return jsonify({success: False, error: 'Order ID not found'})
     print(orderid)
     for card in recipientCardJSON: 
         print(card['recipientEmail'])
         recipient = get_users_by_email(card['recipientEmail'])
         cursor.execute('insert into order_recipients (order_id_fk, recipient_users_fk, gift_msg) values (%s, %s, %s)', (orderid, recipient[0][0], card['message'])) 
-    response = jsonify(orderid)
+        cursor.execute('select MAX(id) AS order_recipient_id FROM order_recipients WHERE order_id_fk = %s', (orderid,))
+        data = cursor.fetchall()
+        order_recipient_id = data[0][0]
+        if (order_recipient_id == 0):
+            return jsonify({success: False, error: 'Order recipient ID not found'})
+        cardValues = card['values']
+        print(cardValues)
+        for key in cardValues:
+            print(key, cardValues[key])
+            cursor.execute('insert into order_recipient_items (order_recipient_fk, product_items_fk, quantity) values (%s, %s, %s)', (order_recipient_id, key, cardValues[key]))
+
+    response = jsonify({success: True})
     return response
 
-
+@app.route('/get_order_info', methods=["GET"])
+def get_order_info():
+    cursor.execute('select o.id order_id, u.email customer_email, or_rec.gift_msg recipient_gift_msg, rec.email recipient_email, pi.name product_name, ori.quantity order_quantity from orders o, users u, users rec, order_recipients or_rec, order_recipient_items ori, product_items pi where o.customer_users_fk = u.id and or_rec.order_id_fk = o.id and or_rec.recipient_users_fk = rec.id and ori.order_recipient_fk = or_rec.id and ori.product_items_fk = pi.id')
+    data = cursor.fetchall()
+    response = jsonify(data)
+    return response
+    
 if __name__ == '__main__':
     app.run(port=5001)
